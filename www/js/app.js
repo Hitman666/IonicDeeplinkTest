f360ed57526e085b3dd680af6e73731538921eac
Nikola <nikola.breznjak@gmail.com>
2017-08-25 09:37:31 +0200

    Finished project

---
+++ www/js/app.js
@@ -0,0 +1,116 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ionic.native'])
+
+.run(function($ionicPlatform, $cordovaDeeplinks, $timeout, $state) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+
+        $cordovaDeeplinks.route({
+            '/chats/:chatId': {
+                target: 'tab.chat-detail',
+                parent: 'tab.chats'
+            },
+            '/account': {
+                target: 'tab.account',
+                parent: 'tab.account'
+            },
+            '/chats': {
+                target: 'tab.chats',
+                parent: 'tab.chats'
+            }
+        }).subscribe(function(match) {
+            console.log('matching');
+            console.dir(match);
+            $timeout(function() {
+                $state.go(match.$route.parent, match.$args);
+
+                if (match.$route.target != match.$route.parent) {
+                    $timeout(function() {
+                        $state.go(match.$route.target, {chatId: match.$args.chatId});
+                    }, 800);
+                }
+            }, 100); // Timeouts can be tweaked to customize the feel of the deeplink
+        }, function(nomatch) {
+            console.warn('No match', nomatch);
+            console.dir(nomatch);
+        });
+    });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+    })
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.dash', {
+        url: '/dash',
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+            }
+        }
+    })
+
+    .state('tab.chats', {
+            url: '/chats',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/tab-chats.html',
+                    controller: 'ChatsCtrl'
+                }
+            }
+        })
+        .state('tab.chat-detail', {
+            params: {chatId: 0},
+            url: '/chats/:chatId',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/chat-detail.html',
+                    controller: 'ChatDetailCtrl'
+                }
+            }
+        })
+
+    .state('tab.account', {
+        url: '/account',
+        views: {
+            'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+            }
+        }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
+
+});
